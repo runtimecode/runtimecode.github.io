ccd2a6333cf5c17af330473ca2b10919886532b1
Prabhat Kumar <prabhatk@adobe.com>
2022-08-19 11:20:34 +0530

    fixed issues

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,112 @@
 import React from "react";
 import "./portfolio.css";
+import p1 from "../../assets/p1.png";
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={p1} alt="" />
+          </div>
+          <h3>This is the portfolio item title</h3>
+
+          <div className="portfolio__item-cta">
+            <a href="" className="btn">
+              Github
+            </a>
+            <a href="" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={p1} alt="" />
+          </div>
+          <h3>This is the portfolio item title</h3>
+
+          <div className="portfolio__item-cta">
+            <a href="" className="btn">
+              Github
+            </a>
+            <a href="" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={p1} alt="" />
+          </div>
+          <h3>This is the portfolio item title</h3>
+
+          <div className="portfolio__item-cta">
+            <a href="" className="btn">
+              Github
+            </a>
+            <a href="" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={p1} alt="" />
+          </div>
+          <h3>This is the portfolio item title</h3>
+
+          <div className="portfolio__item-cta">
+            <a href="" className="btn">
+              Github
+            </a>
+            <a href="" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={p1} alt="" />
+          </div>
+          <h3>This is the portfolio item title</h3>
+
+          <div className="portfolio__item-cta">
+            <a href="" className="btn">
+              Github
+            </a>
+            <a href="" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={p1} alt="" />
+          </div>
+          <h3>This is the portfolio item title</h3>
+
+          <div className="portfolio__item-cta">
+            <a href="" className="btn">
+              Github
+            </a>
+            <a href="" className="btn btn-primary" target="_blank">
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,112 @@
 import React from "react";
 import "./services.css";
+import { BiCheck } from "react-icons/bi";
 
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>Backend Development</h3>
+          </div>
+          <div className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+          </div>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <div className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+          </div>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Backend Development</h3>
+          </div>
+          <div className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem, Ipsem dolor sit amet consetetur elit....</p>
+            </li>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
